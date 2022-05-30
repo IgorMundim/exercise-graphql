07a9f4d43d13b21e6172e40f8070deabfab62b67
Igor Mundim <igormundim@github.com>
2022-05-30 15:55:32 -0300

    first commit

---
+++ Ex 7/client.js
@@ -0,0 +1,35 @@
+// web, curl, isominia, postman
+// mutation {
+//     createMessage(input: {
+//       author: "andy",
+//       content: "hope is a good thing",
+//     }) {
+//       id
+//     }
+//   }
+var author = 'andy';
+var content = 'hope is a good thing';
+var query = `mutation CreateMessage($input: MessageInput) {
+  createMessage(input: $input) {
+    id
+  }
+}`;
+
+fetch('/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({
+    query,
+    variables: {
+      input: {
+        author,
+        content,
+      }
+    }
+  })
+})
+  .then(r => r.json())
+  .then(data => console.log('data returned:', data));
